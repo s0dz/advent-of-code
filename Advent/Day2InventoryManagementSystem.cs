f875688e5604a635d6183e014b791e2ea5db03b2
Garrett McTear <garrett.mctear@gmail.com>
2018-12-05 16:56:58 -0700

    Day 2: Part 1

--- Advent/Day2InventoryManagementSystem.cs
+++ Advent/Day2InventoryManagementSystem.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Advent
 {
-    class Day2InventoryManagementSystem
+    public class Day2InventoryManagementSystem
     {
+        /*
+         * https://adventofcode.com/2018/day/2
+         */
+        // https://en.wikipedia.org/wiki/Checksum
+        public static int ChecksumFunction(List<string> inputs)
+        {
+            var twoCount = 0;
+            var threeCount = 0;
+
+            foreach (var input in inputs)
+            {
+                if (input.GroupBy(character => character).FirstOrDefault(character => character.Count() == 2 ) != null)
+                {
+                    twoCount++;
+                }
+                if (input.GroupBy(character => character).FirstOrDefault(character => character.Count() == 3) != null)
+                {
+                    threeCount++;
+                }
+            }
+
+            return twoCount * threeCount;
+        }
     }
 }
